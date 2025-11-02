948018b01fdba379a239748d40bc4eaa6ace6095
Melvinj1013 <checkthemailbox1013@gmail.com>
2025-11-03 00:30:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// This file is currently empty.
+// We will add JavaScript code here later for functionality like
+// shopping cart interactions, form validation, and mobile menu toggles.
+// For now, it serves as a placeholder linked by index.html.
